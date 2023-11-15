ec6181a080941624dea1800474fb3e6cbc674974
satya <a.satya.syahputra@gmail.com>
2023-11-15 15:35:26 +0700

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# MYSQL TEST CONNECTION TIME PERFORMANCE
+Run
+```
+QUERY="select * from you_table;" node app.js
+```
---
+++ app.js
@@ -0,0 +1,46 @@
+// Import the required module
+const mysql = require('mysql');
+
+// Create a connection to the MySQL database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'column_level_enc',
+});
+
+console.log(process.env.QUERY)
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+
+  let startTime = performance.now();
+  // Perform a simple query
+  connection.query(`${process.env.QUERY}`, (queryErr, results) => {
+    if (queryErr) {
+      console.error('Error executing query:', queryErr);
+      return;
+    }
+
+    // Log the query results
+    // console.log('Query Results:', results);
+    let endTime = performance.now();
+    let time = endTime - startTime
+
+    console.log(time);
+
+    // Close the connection
+    connection.end((endErr) => {
+      if (endErr) {
+        console.error('Error closing connection:', endErr);
+        return;
+      }
+      console.log('Connection closed');
+    });
+  });
+});
